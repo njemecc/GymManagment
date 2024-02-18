b6942e8a4152598b904fb5aa26ab3419f9038610
brani <branislannjemec@gmail.com>
2024-02-18 20:51:20 +0100

    create SubscriptionType.cs

---
+++ GymManagement.Contracts/Subscriptions/SubscriptionType.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GymManagment.Contracts.Subscriptions;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum SubscriptionType
+{
+
+Free,
+Starter,
+Pro
+}
