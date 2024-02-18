b5d790621fa1aacbd3f11e9b6fdd6ee626d6f281
brani <branislannjemec@gmail.com>
2024-02-18 20:50:13 +0100

    create DependencyInjection files for Application and Infrastructure layer

---
+++ GymManagment.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GymManagment.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssemblyContaining(typeof(DependencyInjection));
+        });
+        return services;
+    }
+
+  
+}
---
+++ GymManagment.Infrastracture/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using GymManagment.Infrastracture;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GymManagment.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastracture(this IServiceCollection services)
+    {
+        return services;
+    }
+
+  
+}
