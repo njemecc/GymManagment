026d70d627665fa68da2b66bfcdd451a9f85b63d
brani <branislannjemec@gmail.com>
2024-02-18 20:51:10 +0100

    create SubscriptionController

---
+++ GymManagment.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,36 @@
+﻿
+using GymManagment.Application.Subscriptions.Commands.CreateSubscription;
+using GymManagment.Contracts.Subscriptions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagment.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class SubscriptionsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SubscriptionsController( IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    [HttpPost]
+    public async  Task<IActionResult> CreateSubscription(CreateSubscriptionRequest request)
+    {
+
+        var command = new CreateSubscriptionCommand(request.SubscriptionType.ToString(),request.AdminId);
+        
+        var createSubscriptionResult = await _mediator.Send(command);
+
+      return  createSubscriptionResult.MatchFirst(
+            guid => Ok(new SubscriptionResponse(guid, request.SubscriptionType)),
+            error => Problem());
+        
+
+    }
+}
